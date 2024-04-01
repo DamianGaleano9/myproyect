ac57725d5a10fb9fda4f6837d2e41f4311734212
Damian Galeano <dj.damian.b@hotmail.com>
2024-04-01 16:51:56 +0200

    Upload the img

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
-import './myCss.css'
+import './myCss.css';
+import image1 from './images/capriaty.jpg';
+import image2 from './images/marcocarola.jpg';
+import image3 from './images/jamie.jpg';
+
 
 
 function App() {
-  let name = "Damian";
-  let box = name
+
   return (
-    <div className="fondo">
-      <h1>React Course</h1>
-      {name}
-      <input value ={box}/>
+    <div className="container">
+      <div className="box">
+        <img src={image1} alt="" />
+      </div>
+
+      <div className="box">
+        <img src={image2} alt="" />
+      </div>
+
+      <div className="box">
+        <img src={image3} alt="" />
+      </div>
+
     </div>
   );
 }
